45cf7fa32a4e47c3ef6ef70e5a7713bb78603b4f
Sagarlambat <32290150+Sagarlambat@users.noreply.github.com>
2020-05-31 16:13:16 +0530

    Update Day9.py

--- Sagar/Day9.py
+++ Sagar/Day9.py
@@ -1,6 +1,6 @@
 #1.Write a function to compute divide by zero and use try/except to catch the exceptions.
 
-'''
+
 def reciprocal(x):
 
     try:
@@ -10,11 +10,11 @@ def reciprocal(x):
         
 x=int(input("Enter you no.: "))
 reciprocal(x) 
-'''
+
 
 #2.Write python program to check  that given number is valid mobile number or not?
 
-'''
+
 import re
 s=input("Enter number: ")
 a=re.compile("(0/91)?[7-9][0-9]{9}")
@@ -22,11 +22,11 @@ if(a.match(s)):
     print ("Valid Number")      
 else : 
     print ("Invalid Number") 
-'''
+
 
 #3.Write python program to check  that given email address is valid or not?
 
-'''
+
 import re
 s=input("Enter email address: ")
 a=re.compile("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$")
@@ -34,11 +34,11 @@ if(a.match(s)):
     print ("Valid E-mail id")      
 else : 
     print ("Invalid E-mail id") 
-'''
+
 
 #4.Write a python program to check given car registration number is valid Maharashtra state registration number or not?
 
-'''
+
 import re
 s=input("Enter car registration number: ")
 a=re.compile("^[MH]{2}[ -][0-9]{1,2}(?: [A-Z])?(?: [A-Z]*)? [0-9]{4}$")
@@ -46,10 +46,10 @@ if(a.match(s)):
     print ("Valid car registration number")      
 else : 
     print ("Invalid car registration number")
-'''
+
 
 #5.Write a python program to decorate arithmetic operations.
-'''
+
 def decorator(f1):
     def operation():
         print("operation starts")
@@ -62,11 +62,11 @@ def mul():
     a=int(input("Enter 1st no: "))
     b=int(input("Enter 2nd no: "))
     print("Multiplication of",a,"&",b,"is",a*b)
-'''
+
 
 #6.Write a python program to generate Fibonacci Numbers upto 100 using generator.
 
-'''
+
 def fibo(x):
     a=1
     b=1
@@ -76,4 +76,3 @@ def fibo(x):
 
 for i in fibo(100):
     print(i)
-'''
