923496c96e0d76810f6b4bb56098872140a422ff
Sagarlambat <32290150+Sagarlambat@users.noreply.github.com>
2020-05-31 12:35:30 +0530

    Update and rename Day 6.py to Day6.py

--- Sagar/Day6.py
+++ Sagar/Day6.py
@@ -1,5 +1,5 @@
 #1.Python program to Swap Keys and Values in Dictionary.
-'''
+
 o={'a':5, 'b':54, 'c': 5430,'d':45430, 'e':5434, 'f': 230}
 d = dict([(value, key) for key, value in o.items()]) 
 print ("Original dictionary : ") 
@@ -9,9 +9,9 @@ print("keys: values")
 for i in d: 
     print(i, " :  ", d[i]) 
 
-'''
+
 #2.Write program to implement Selection sort.
-'''
+
 a=[65,42,31,98,7,548,200,354,629,5654,1,2,0]
 for i in range(len(a)):
     for j in range(i+1,len(a)):
@@ -21,11 +21,11 @@ for i in range(len(a)):
             a[j]=x
     print(a)
 print(a)
-'''
+
 
 
 #3.Write program to implement Insertion sort.
-'''
+
 a=[5,3,0,1,7,9,8,2,4,6]
 b=[]
 
@@ -40,10 +40,6 @@ for i in range(1, len(a)):
   
 for i in range(len(a)): 
     print ("%d" %a[i],end="")
-'''
-
-
-
 
 #4.Given a nested list extend it with adding sub list ["h", "i", "j"] in a such a way that it will look like the following list.
 #Given list: list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m",  "n"]
@@ -51,12 +47,12 @@ for i in range(len(a)):
 #Expected output:-
 #['a', 'b', ['c', ['d', 'e', ['f', 'g', 'h', 'i', 'j'], 'k'], 'l'], 'm', 'n']
 
-'''
+
 list1=['a', 'b', ['c', ['d', 'e', ['f', 'g'], 'k'], 'l'], 'm', 'n']
 sublist=['h','i','j']
 list1[2][1][2].extend(sublist)
 print(list1)
-'''
+
 
 #5.Access the value of key “history”
 #sampleDict = { 
@@ -71,7 +67,7 @@ print(list1)
 #   }
 #}
 #Expected output: 80
-'''
+
 sampleDict = { 
    "class":{ 
       "student":{ 
@@ -84,4 +80,3 @@ sampleDict = {
    }
 }
 print(sampleDict['class']['student']['marks']['history'])
-'''
